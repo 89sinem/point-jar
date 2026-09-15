6fcbc360ca6232ba5a903ef7648a22772b35ddc3
89sinem <89ataksinem@gmail.com>
2026-09-15 11:44:34 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Keeps Point Jar working with no internet at all.
    Bump CACHE when the app changes so phones pick the new version up. */
 
-var CACHE = "point-jar-v14";
+var CACHE = "point-jar-v15";
 
 var FILES = [
   "./",
